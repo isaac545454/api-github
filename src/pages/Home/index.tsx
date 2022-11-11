4f3aa663f45e205342b67dcd90c9eb41b88da567
isaac545454 <isaac.newton1221@gmail.com>
2022-11-11 16:57:32 -0300

    input ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,17 +3,23 @@ import { FaGithub, FaPlus } from "react-icons/fa";
 
 export default function index() {
   return (
-    <div className="max-w-[700px] flex flex-col items-center bg-white rounded p-8 pt-10 mx-auto">
-      <h1 className="text-">
-        <FaGithub size={25} />
-        indaaaaaaaaaaaaaaaaaaaaaaex
-      </h1>
-      <form>
-        <input type="text" placeholder="Adicionar repositorio" />
-        <button>
-          <FaPlus color="#fff" size={14} />
-        </button>
-      </form>
+    <div className="">
+      <div className="max-w-[700px]  bg-white rounded p-8 mt-20 mx-auto">
+        <h1 className="text-xl flex  items-center">
+          <FaGithub size={25} className="mr-2" />
+          indaaaaaaaaaaaaaaaaaaaaaaex
+        </h1>
+        <form className="mt-8 flex flex-row">
+          <input
+            type="text"
+            placeholder="Adicionar repositorio"
+            className="flex-1 border-[#ddd] border-[4px] text-lg rounded-md g px-4 py-3"
+          />
+          <button className="bg-[#0d2636] rounded ml-3 py-0 px-5 flex items-center justify-center">
+            <FaPlus color="#fff" size={14} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
